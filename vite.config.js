3fb311dcf56ecfc07289ec2f0d786bafa7f74834
Monopavo <monopavomedia@gmail.com>
2024-10-31 16:46:32 +0000

    Hotfix: Renamed base in config.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/100-days-todo-list/",
+  base: "/100-days-todo-app/",
   plugins: [react()],
 })
 
